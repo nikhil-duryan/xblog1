9b38e6ec10e7edce7a620fdc28c665832e3485e4
nikhil-duryan <87350951+nikhil-duryan@users.noreply.github.com>
2022-03-05 10:25:08 +0530

    Update urls.py

--- XBlog/urls.py
+++ XBlog/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Home.urls')),
     path('Blog/', include('Blog.urls')),
-    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
