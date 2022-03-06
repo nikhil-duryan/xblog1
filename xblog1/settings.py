10644bb084e536262c5c52cbd3bacbb3f4fa301e
nikhil-duryan <87350951+nikhil-duryan@users.noreply.github.com>
2022-03-06 11:34:59 +0530

    Update settings.py

--- xblog1/settings.py
+++ xblog1/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+dsfmw=!hff5@^$x+2p&ebz!@#u&2g5t$s#da11p@xl=l%a@81'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','xblog1.herokuapp.com']
 
